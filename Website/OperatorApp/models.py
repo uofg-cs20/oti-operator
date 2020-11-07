1bd5442c6e5aeec984b71d2197cf44d4ee655714
Tomas Mikus <tomas.mikus@outlook.com>
2020-11-07 19:01:50 +0000

    Add more attributes to the operator model. No password/admin yet

--- Website/OperatorApp/models.py
+++ Website/OperatorApp/models.py
@@ -3,6 +3,14 @@ from django.db import models
 class Operator(models.Model):
     name = models.CharField(max_length=200)
     modes = models.ManyToManyField('Mode')
+    
+    homepage = models.URLField()
+    api_url = models.URLField()
+    default_language = models.CharField(max_length=40)
+    phone = models.IntegerField(max_length=20)
+    email = models.EmailField()
+    active = models.BooleanField()
+
 
     def __str__(self):
         return self.name
