5550c0b0859132d3a5540e860f5bce2795944697
Tomas Mikus <tomas.mikus@outlook.com>
2020-11-07 18:49:38 +0000

    Create basic models for the operator

--- Website/OperatorApp/models.py
+++ Website/OperatorApp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Operator(models.Model):
+    name = models.CharField(max_length=200)
+    modes = models.ManyToManyField('Mode')
+
+    def __str__(self):
+        return self.name
+
+
+
+class Mode(models.Model):
+    short_desc = models.CharField(max_length=50)
+    long_desc = models.CharField(max_length=8000)
+
+    def __str__(self):
+        return self.short_desc
