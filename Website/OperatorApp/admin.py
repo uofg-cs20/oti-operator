cd56ce415b3b73843ff1cf07f4a4c94d856b73b6
Tomas Mikus <tomas.mikus@outlook.com>
2020-11-07 23:47:31 +0000

    Display more info on the django admin site

--- Website/OperatorApp/admin.py
+++ Website/OperatorApp/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 
 from .models import Operator, Mode
 
-admin.site.register(Operator)
-admin.site.register(Mode)
+class OperatorAdmin(admin.ModelAdmin):
+    list_display = ('admin', 'name', 'get_modes', 'homepage', 'api_url', 'default_language', 'phone', 'email', 'active')
+
+    def get_modes(self, obj):
+        return "\n".join([mode.short_desc for mode in obj.modes.all()])
+
+class ModeAdmin(admin.ModelAdmin):
+    list_display = ('short_desc', 'long_desc')
+
+admin.site.register(Operator, OperatorAdmin)
+admin.site.register(Mode, ModeAdmin)
