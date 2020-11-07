174ea915c3b7ab2143681671fbc5228ce8b05789
Tomas Mikus <tomas.mikus@outlook.com>
2020-11-07 23:46:56 +0000

    Add admin field to the operator profile

--- Website/OperatorApp/models.py
+++ Website/OperatorApp/models.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 class Operator(models.Model):
+    admin = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     modes = models.ManyToManyField('Mode')
-    
     homepage = models.URLField()
     api_url = models.URLField()
     default_language = models.CharField(default='English', max_length=40)
@@ -20,6 +22,7 @@ class Operator(models.Model):
 class Mode(models.Model):
     short_desc = models.CharField(max_length=50)
     long_desc = models.CharField(max_length=8000, null=True)
+
 
     def __str__(self):
         return self.short_desc
