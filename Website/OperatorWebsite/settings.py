3fe4e37ce395fd4a23333bc2befa00749193a45b
Tomas Mikus <tomas.mikus@outlook.com>
2020-11-07 18:49:10 +0000

    Fix typo

--- Website/OperatorWebsite/settings.py
+++ Website/OperatorWebsite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'OperatorApp.apps.OperatorAppConfig',
+    'OperatorApp.apps.OperatorappConfig',
 ]
 
 MIDDLEWARE = [
