80c5c572e7f4abe8f69c6179aede2d04f110f6f9
Tomas Mikus <tomas.mikus@outlook.com>
2020-11-07 20:08:53 +0000

    Create a template for the homepage

--- Website/OperatorApp/views.py
+++ Website/OperatorApp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Homepage")
+    return render(request, 'OperatorApp/index.html')
