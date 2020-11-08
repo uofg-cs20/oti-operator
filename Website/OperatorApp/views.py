806735325d39d30b92fed996665ec190ac713cc7
Tomas Mikus <tomas.mikus@outlook.com>
2020-11-08 00:00:41 +0000

    Add stub functions for application views

--- Website/OperatorApp/views.py
+++ Website/OperatorApp/views.py
@@ -3,3 +3,23 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'OperatorApp/index.html')
+
+# login operator
+def login(request): 
+    pass
+
+# log out operator
+def logout(request):
+    pass
+
+# display operator profile
+def profile(request):
+    pass
+
+# edit operator profile
+def edit_profile(request):
+    pass
+
+# display all operators (when an operator logs in)
+def operators(request):
+    pass
