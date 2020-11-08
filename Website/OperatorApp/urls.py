57d8b22e4a6b73fa470aca893bb12338fc97e2d6
Tomas Mikus <tomas.mikus@outlook.com>
2020-11-08 00:42:20 +0000

    Update website urls

--- Website/OperatorApp/urls.py
+++ Website/OperatorApp/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views 
 
+app_name = 'operator'
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('edit/', views.edit_profile, name='edit'),
 ]
