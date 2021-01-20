519cdfcf79bf3a3dd4b32e0e45472fa5a91d48e6
Kameron <2386181e@student.gla.ac.uk>
2021-01-20 22:49:38 +0000

    updated test to work

--- Website/OperatorApp/tests/operator_api_test.py
+++ Website/OperatorApp/tests/operator_api_test.py
@@ -22,25 +22,31 @@ class OperatorTestCase(TestCase):
         self.operator1.modes.add(on_foot.id)
 
     def test_operator_api(self):
-        print("Operator API Test")
+        print('Operator API Test')
         response = self.client.get('/api/?operator=1')
         content = json.loads(response.content)
-        comparisonDict = {"model": "OperatorApp.operator",
-                          "pk": 1, "fields": {"admin": 1, "name": "TestOperator",
-                                              "homepage": "https://operator.co.uk",
-                                              "api_url": "https://test.opentransport.co.uk",
-                                              "default_language": "English", "phone": "07083249084",
-                                              "email": "test@operator.co.uk", "active": True, "modes": [1]}}
+        comparisonDict = {'catalogue-metadata': [
+            {'rel': 'urn:X-hypercat:rels:isContentType', 'val': 'application/vnd.hypercat.catalogue+json'},
+            {'rel': 'urn:X-hypercat:rels:hasDescription:en', 'val': 'OpenTransport Operator Catalogue'},
+            {'rel': 'urn:X-hypercat:rels:supportsSearch', 'val': 'urn:X-hypercat:search:simple'}], 'items': [
+            {'href': 'https://operator.co.uk0', 'item-metadata': [
+                {'rel': 'urn:X-hypercat:rels:isContentType', 'val': 'application/vnd.hypercat.catalogue+json'},
+                {'rel': 'urn:X-hypercat:rels:hasDescription:en', 'val': 'TestOperator'},
+                {'rel': 'hasHomepage', 'val': 'https://operator.co.uk'}, {'rel': 'hasID', 'val': 1},
+                {'rel': 'hasEmail', 'val': 'test@operator.co.uk'}, {'rel': 'hasPhone', 'val': '07083249084'},
+                {'rel': 'hasDefaultLanguage', 'val': 'English'}, {'rel': 'hasNumberModes', 'val': 1},
+                {'rel': 'hasNumberMode#Code1', 'val': 1}, {'rel': 'hasNumberMode#Description1', 'val': 'on foot'},
+                {'rel': 'api_url', 'val': 'https://test.opentransport.co.uk'}]}]}
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(content, comparisonDict)
 
     def test_mode_api(self):
-        print("Mode API Test")
+        print('Mode API Test')
         response = self.client.get('/api/?mode=1')
         content = json.loads(response.content)
-        comparisonDict = {'model': 'OperatorApp.mode', 'pk': 1,
-                          'fields': {'short_desc': 'on foot', 'long_desc': 'for complete end-to-end journey mapping'}}
+        comparisonDict = {"modes": [{"model": "OperatorApp.mode", "pk": 1, "fields":
+            {"short_desc": "on foot", "long_desc": "for complete end-to-end journey mapping"}}]}
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(content, comparisonDict)
